4df5062746661241fed0c626ce3c3a20fb1ddca7
sigvant <bnsviv@gmail.com>
2021-10-15 15:57:41 -0300

    before setting the context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
 import { Link } from 'react-router-dom'
+import Services from '../components/Services'
 
 function Home() {
     return (
-        <Hero>
-            <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $199'>
-                <Link to='/rooms'className='btn-primary'>
-                    our rooms
-                </Link>
-            </Banner>
-        </Hero>
+        <>
+            <Hero>
+                <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $199'>
+                    <Link to='/rooms'className='btn-primary'>
+                        our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            <Services/>
+        </>
     )
 }
 
