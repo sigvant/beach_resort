1f82dba5d9631bd37695e2aba325ef1109d0b811
sigvant <bnsviv@gmail.com>
2021-11-19 18:12:05 -0300

    removing exposed keys

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-    space: "jfe6ighcgbkz",
-    accessToken: "wS6Fe-3nQIpWPhd0VevkD9aSfWS--jyxVHx91huso5c"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
